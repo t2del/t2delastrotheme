63f137ab3a417fbb0515d17c415ecf31bf069bd8
t2del <macapagal.dennis@gmail.com>
2023-09-21 23:35:37 +0800

    first commit

---
+++ src/config/nav.js
@@ -0,0 +1,14 @@
+export const nav = [
+	{
+		title: 'Home',
+		slug: '/',
+	},
+	{
+		title: 'About',
+		slug: '#about',
+	},
+	{
+		title: 'Portfolio',
+		slug: '/#portfolio',
+	},
+];
---
+++ src/config/settings.js
@@ -0,0 +1,8 @@
+export default {
+	title: `T2del`,
+	description: `T2del`,
+	url: `#`, // No trailing slash!
+	name: `#`, // The short name of the business or brand name. Used for things like the copyright in the footer.
+  enableThemeSwitcher: true,
+  showLittleSticksPlug: true, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
+};
